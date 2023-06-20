f2296de2bddbd87c484acc71717398d592e23fb6
rcmadden <dataengineer007@gmail.com>
2023-06-20 15:34:00 -0700

    first in-efficient attempt

--- requirements.md
+++ requirements.md
@@ -3,12 +3,12 @@
 Stanford IMDB Movie Reviews data set: dataSentences.txt
 [x]Skip first line (header: sentence, sentence number)
 [x]Skip line numbers (first character on each line)
-[]Only about 20,000 distinct words
+Only about 20,000 distinct words
 ## Write a Dataset Class
-[]All our code should be contained in its own class
+[x]All our code should be contained in its own class
 [x]Constructor takes filename as input
-[]Need variables for the following: n_ sentences, vocab (all the distinct words), _words, all the sentences
-[]Dict. To map each word to a unique integer index
+[x]Need variables for the following: n_ sentences, vocab (all the distinct words), _words, all the sentences
+[x]Dict. To map each word to a unique integer index
 [x]Function to read file - sentences as [strings] (lowercase)
-[]Function to compile vocabulary of unique words to a list (return vocab, n_ words)
-[]Function to get word frequencies as a dictionary of key for word number of occurances as value
+[x]Function to compile vocabulary of unique words to a list (return vocab, n_ words)
+[x]Function to get word frequencies as a dictionary of key for word number of occurances as value
--- textParser.py
+++ textParser.py
@@ -1,17 +1,50 @@
-
+import re
+from collections import Counter 
 class Dataset:
-    def __init__(self, input_file):
-        self.input_file = input_file
+    def __init__(self, filename):
+        self.filename = filename
         self.n_sentences = []
-        self.vocab = None
-        self.words = {}
+        self.vocab = []
+        self.words = []
+        self.n_words = 0
 
 
-def read_file():
-    n_sentences = []
-    with open('datasetSentences.txt', 'r') as file:
-        file.readline() # skip first row
-        lines = file.readlines()
-        for line in lines:
-            n_sentences.append(line.lstrip('0123456789\t'))
-    return
+    def read_file(self):
+        # n_sentences = []
+        with open(self.filename, 'r') as file:
+            file.readline() # skip first row
+            lines = file.readlines()
+            for line in lines:
+                self.n_sentences.append(line.lstrip('0123456789\t').rstrip('\n').lower())
+        return self.n_sentences
+    
+    def get_unique_words(self):
+        for sentence in self.n_sentences:
+            for word in sentence.split():
+                # if word not in self.words:
+                self.words.append(re.sub(r'\W+', '', word))
+        self.vocab = list(set(self.words))
+        self.n_words = len(self.vocab)
+        return self.vocab, self.n_words
+
+    def get_word_counts(self):
+        word_counter = Counter()
+        for sentence in self.n_sentences:
+            words = re.findall(r'\w+', sentence)
+            word_counter.update(words)
+        for word, count in word_counter.most_common(10):
+            print(word, count)
+        return word_counter
+
+
+file_handler = Dataset('datasetSentences.txt')
+file_content = file_handler.read_file()
+# print(len(file_content))
+# print(file_content[100])
+word_list, word_count = file_handler.get_unique_words()
+# print(word_list[100])
+# print(word_count)
+count_dict = file_handler.get_word_counts()
+
+print(type(count_dict))
+
